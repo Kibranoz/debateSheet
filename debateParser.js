6f50a8ebae9f5bd4b5013133cb09dd8e60c48f93
Kibranoz <35783229+Kibranoz@users.noreply.github.com>
2023-06-11 15:02:15 -0400

    added debateParser

---
+++ debateParser.js
@@ -0,0 +1,61 @@
+class DebateParser {
+    constructor(debateSheetInfo) {
+        this.debateSheetInfo = debateSheetInfo;
+    }
+    getTitle() {
+        const title = document.getElementById("motionTitle").value;
+        const titleString = "Qu'il soit résolu que " + title;
+        return this.getHeader(titleString);
+    }
+    getHeader(headerTitle) {
+        const header = "=".repeat(headerTitle.length);
+        return header + "\n" + headerTitle +  "\n" + header + "\n";
+    }
+    getIntro() {
+        if (this.debateSheetInfo.isGov) {
+       let introString = "Contexte dans lequel la motion se situe : " + document.getElementById("contextContent").value + "\n";
+       introString += "Caractérisation : " + document.getElementById("caracterisationContent").value + "\n";
+       introString += "Notre point de vue : " + document.getElementById("summaryContent").value + "\n" 
+       return introString;
+    }
+    }
+    getArguments() {
+        let argumentsString = "";
+        for (let i=1; i< this.debateSheetInfo.numberOfArguments; i++) {
+            let argumentParts = document.querySelectorAll('[argNo="'+i+'"]');
+            argumentsString += this.getHeader("Argument "+ i + " : " + argumentParts[0].value);
+            argumentsString += "Prémisses : " + argumentParts[1].value + '\n';
+            argumentsString += "Mécanisme : " + argumentParts[2].value + '\n';
+            argumentsString += "Impacts : " + argumentParts[3].value + '\n';
+        }
+        return argumentsString;
+    }
+
+    getRefutations() {
+        let whatTheySaid = Array.from(document.querySelectorAll(".them")).map((elem) => elem.value);
+        let whatWeSaid = Array.from(document.querySelectorAll(".us")).map((elem) => elem.value);
+        let refutString = this.getHeader("Réfutations")
+        whatTheySaid.forEach((element,index) => {
+            refutString += "L'équipe adverse a dit : " + element + "\n";
+            refutString += "Ce a quoi nous avons répondu : " + whatWeSaid[index] + "\n";
+        });
+        return refutString;
+    }
+
+    getCristal() {
+        let cristalString = this.getHeader("Cristallisation")
+        cristalString+= "Dans ce débat nous avons parlé des thèmes suivants : " + document.getElementById("themeContent").value + "\n";
+        cristalString += "Ce que l'autre coté dit : " + document.getElementById("otherContent").value + "\n";
+        cristalString += "Ce que nous disons : " + document.getElementById("nousContent").value + "\n";
+        cristalString += "Nous avons raison parce que : " + document.getElementById("conclusionContent").value + "\n";
+        return cristalString;
+    }
+
+    parseAll() {
+        return this.getTitle()+ 
+        this.getIntro() + 
+        this.getArguments() +
+        this.getRefutations() + 
+        this.getCristal();
+    }
+}
--- source.js
+++ source.js
@@ -1,20 +1,77 @@
-var argumentNo = 1;
+class DebateSheet {
+    constructor() {
+        this.argumentNo = 1;
+        this.isGovernement = true;
 
-function createNewArgument() {
-    let arguments = document.getElementById("arguments");
-    arguments.appendChild(createLabelledInput("input","argTitle", "Argument "+argumentNo));
-    argumentNo ++;
-    arguments.appendChild(createLabelledInput("textarea","premiss", "Prémisse"))
-    arguments.appendChild(createLabelledInput("textarea", "mecha", "Mécanisation"))
-    arguments.appendChild(createLabelledInput("textarea", "impact", "Impact"));
+        document.getElementById("addArg").addEventListener("click", ()=> {
+            this.createNewArgument()
+        })
+        
+        document.getElementById("addRefut").addEventListener("click", ()=> {
+            this.addRefutations()
+        })
+        
+        document.getElementById("govButton").addEventListener("click", ()=> {
+            Array.from(document.getElementsByClassName("government")).forEach(element => {
+                element.classList.remove("hidden")
+            });
+            this.isGov = true;
+        })
+        
+        document.getElementById("oppButton").addEventListener("click", ()=> {
+            console.log(document.getElementsByClassName("government"));
+            Array.from(document.getElementsByClassName("government")).forEach(element => {
+                console.log(element)
+                element.classList.add("hidden")
+            });
+            this.isGov = false;
+        })
+
+        document.getElementById("saveButton").addEventListener("click", async ()=> {
+            const debateParser = new DebateParser(this.toDto());
+
+            let fileHandle;
+            // Destructure the one-element array.
+            fileHandle = await window.showSaveFilePicker({
+              types: [
+                {
+                  description: 'Text Files',
+                  accept: {
+                    'text/plain': ['.txt'],
+                  },
+                },
+              ],
+            });
+            // Do something with the file handle.
+          
+          const contents =  debateParser.parseAll();
+          
+          const writable = await fileHandle.createWritable();
+            // Write the contents of the file to the stream.
+            await writable.write(contents);
+            // Close the file and write the contents to disk.
+            await writable.close();      
+              })
+    }
+createNewArgument() {
+    let debateArguments = document.getElementById("arguments");
+    debateArguments.appendChild(this.createLabelledInput("input","argTitle", "Argument "+ this.argumentNo, this.argumentNo));
+    debateArguments.appendChild(this.createLabelledInput("textarea","premiss", "Prémisse", this.argumentNo))
+    debateArguments.appendChild(this.createLabelledInput("textarea", "mecha", "Mécanisation", this.argumentNo))
+    debateArguments.appendChild(this.createLabelledInput("textarea", "impact", "Impact", this.argumentNo));
+    this.argumentNo ++;
+
 }
 
-function createLabelledInput(type, label, text) {
+createLabelledInput(type, label, text, number=0) {
     let labelItem = document.createElement('label');
     labelItem.setAttribute("for", label);
     labelItem.innerText = text
     let textArea = document.createElement(type);
     textArea.setAttribute("name", label);
+    if (number!=0) {
+        textArea.setAttribute("argNo", number)
+    }
     let container = document.createElement("div");
     container.classList.add("argument");
     container.appendChild(labelItem);
@@ -22,39 +79,30 @@ function createLabelledInput(type, label, text) {
     return container;
 }
 
-function createUnlabelledTextArea() {
+createUnlabelledTextArea(attClass) {
     let textArea = document.createElement("textArea");
+    textArea.classList.add(attClass);
     return textArea;
 }
 
-function addRefutations() {
+addRefutations() {
     let refutations = document.getElementById("refutations");
     let refutation = document.createElement("div");
     refutation.classList.add("refutation");
-    refutation.appendChild(createUnlabelledTextArea());
-    refutation.appendChild(createUnlabelledTextArea());
+    refutation.appendChild(this.createUnlabelledTextArea("them"));
+    refutation.appendChild(this.createUnlabelledTextArea("us"));
     refutations.appendChild(refutation);
 }
 
-document.getElementById("addArg").addEventListener("click", ()=> {
-    createNewArgument()
-})
+toDto() {
+    return {
+        isGov : this.isGovernement,
+        numberOfArguments: this.argumentNo
+    }
+}
 
-document.getElementById("addRefut").addEventListener("click", ()=> {
-    addRefutations()
-})
 
-document.getElementById("govButton").addEventListener("click", ()=> {
-    Array.from(document.getElementsByClassName("government")).forEach(element => {
-        element.classList.remove("hidden")
-    });
-})
+}
 
-document.getElementById("oppButton").addEventListener("click", ()=> {
-    console.log(document.getElementsByClassName("government"));
-    Array.from(document.getElementsByClassName("government")).forEach(element => {
-        console.log(element)
-        element.classList.add("hidden")
-    });
-})
 
+let controller = new DebateSheet();
